696ec9e9a71080ea1feeb980fcdb16dbb17648e8
DavidRamalho86 <david.ramalho@aluno.senai.br>
2024-02-15 15:19:53 -0300

    .

---
+++ 1.js
@@ -0,0 +1,15 @@
+function calcularImposto) {
+    var valorCompra = parseFloat(document.getElementById("valorCompra").value);
+    var desconto = 0;
+
+    if (valorCompra >= 3000) {
+        desconto = 0.1;
+    } else if (valorCompra >= 2501 && valorCompra <= 2999) {
+        desconto = 0.05;
+    }
+
+    var valorFinal = valorCompra - (valorCompra * desconto);
+
+    document.getElementById("resultado").textContent = `O valor da compra é R$${valorCompra.toFixed(2)}. O desconto aplicado é de ${desconto * 100}%. O valor final a ser pago é R$${valorFinal.toFixed(2)}.`;
+}
+
